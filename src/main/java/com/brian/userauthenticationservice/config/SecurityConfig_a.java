d55a3b02e7aea36ff15d50031bb7fef56a3818af
BrianSam <briansamgames@gmail.com>
2025-11-27 16:35:16 +0530

    added spring auth server mvc and dependencies

---
+++ src/main/java/com/brian/userauthenticationservice/config/SecurityConfig_a.java
@@ -0,0 +1,20 @@
+package com.brian.userauthenticationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig_a {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable())     // New syntax
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().permitAll()
+//                );
+//
+//        return http.build();
+//    }
+}
--- src/test/java/com/brian/userauthenticationservice/UserAuthenticationServiceApplicationTests.java
+++ src/test/java/com/brian/userauthenticationservice/UserAuthenticationServiceApplicationTests.java
@@ -1,13 +1,46 @@
 package com.brian.userauthenticationservice;
 
+import com.brian.userauthenticationservice.config.authConfig.repositoriy.ClientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+import java.util.UUID;
 
 @SpringBootTest
 class UserAuthenticationServiceApplicationTests {
 
+    @Autowired
+    RegisteredClientRepository registeredClientRepository;
+
     @Test
     void contextLoads() {
     }
+//    @Test
+//    void test(){
+//
+//
+//        	RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+//				.clientId("brian")
+//				.clientSecret("{bcrypt}secret")
+//				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//				.redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+//				.postLogoutRedirectUri("http://127.0.0.1:8080/")
+//				.scope(OidcScopes.OPENID)
+//				.scope(OidcScopes.PROFILE)
+//				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+//				.build();
+//
+//            registeredClientRepository.save(oidcClient);
+//    }
 
 }
